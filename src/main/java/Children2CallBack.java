d9d135351c63eb08ba898604d03d0727e70d9864
DESKTOP-B42P468\GYX <904985041@qq.com>
2018-04-18 11:35:41 +0800

    添加查询子节点demo

---
+++ src/main/java/Children2CallBack.java
@@ -0,0 +1,19 @@
+import org.apache.zookeeper.AsyncCallback;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.List;
+
+public class Children2CallBack implements AsyncCallback.Children2Callback {
+
+	@Override
+	public void processResult(int rc, String path, Object ctx, List<String> children, Stat stat) {
+		for (String s : children) {
+			System.out.println(s);
+		}
+		System.out.println("ChildrenCallback:" + path);
+		System.out.println((String)ctx);	
+		System.out.println(stat.toString());
+	}
+
+
+}
---
+++ src/main/java/ChildrenCallBack.java
@@ -0,0 +1,16 @@
+import org.apache.zookeeper.AsyncCallback;
+
+import java.util.List;
+
+public class ChildrenCallBack implements AsyncCallback.ChildrenCallback {
+
+	@Override
+	public void processResult(int rc, String path, Object ctx, List<String> children) {
+		for (String s : children) {
+			System.out.println(s);
+		}
+		System.out.println("ChildrenCallback:" + path);
+		System.out.println((String)ctx);	
+	}
+
+}
